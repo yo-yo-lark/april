88d8deecda12ebbe4d86b2e07c1c70f62cc568ac
Bob <bob@example.com>
2023-10-19 15:51:13 -0600

    step 2 created function

--- temp.py
+++ temp.py
@@ -1,3 +1,12 @@
-# Program to convert Celsius To Fahrenheit
-fah = 25 * 9/5 + 32
-print(f"25 degress is {fah} Fahrenheit")
+"""
+    Program to convert Celsius To Fahrenheit
+"""
+def celsius_to_fahrenheit(celsius):
+    """ convert value given to fahrenheit"""
+    fah = celsius * 9/5 + 32
+    return fah
+
+
+celsius = 25
+fah = celsius_to_fahrenheit(celsius)
+print(f"{celsius} degress is {fah} Fahrenheit")
